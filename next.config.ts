e1e9dd77fd30f7ca06d9f9abaa58ff8b89211b37
daxjparmar <dax.j.parmar@gmail.com>
2026-08-22 16:51:25 +0530

    Added Configration

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+module.exports = {
+  allowedDevOrigins: ['10.23.4.161'],
+}
+
+export default nextConfig;
